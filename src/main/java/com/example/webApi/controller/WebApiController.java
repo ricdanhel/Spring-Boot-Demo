8d3a861e3dd8ef036a9421f5eade05600d8b1e38
OkhuijsR <ricardo.okhuijsen@hotmail.com>
2019-02-05 21:56:03 +0100

    @CrossOrigin toegevoegd vanwege error wanneer je een aanroep wilt doen vanuit 1 domein naar een andere (met andere port) Controller roept nu de web api aan om alle landen(1) en een ingevoerd land op te halen(2)

--- src/main/java/com/example/webApi/controller/WebApiController.java
+++ src/main/java/com/example/webApi/controller/WebApiController.java
@@ -3,6 +3,7 @@ package com.example.webApi.controller;
 import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,26 +11,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+@CrossOrigin
 @RestController
 public class WebApiController {
-
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public String sayHi(@PathVariable String name) {
-		return "Hoi " + name;
-	}
 	
-	@GetMapping("/country/{countryName}")
+	@RequestMapping(value = "/country/{countryName}", method = RequestMethod.GET)
 	public String getCountryInfo(@PathVariable String countryName) throws IOException {
 		String uri = "https://restcountries.eu/rest/v2/name/" + countryName;
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
 		
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode root = mapper.readTree(response.getBody());
-		JsonNode name = root.findPath("name");		
+		return response.getBody();
+	}
+	
+	@GetMapping("/countries/all")
+	public String getCountries() throws IOException {
+		String uri = "https://restcountries.eu/rest/v2/all";
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
 		
 		return response.getBody();
 	}
